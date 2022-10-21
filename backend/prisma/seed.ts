c0763d73534139ff37762315a77a9129dea9f25d
Mateus Bentes <mateus1337bentes@gmail.com>
2022-10-21 16:41:39 -0400

    chore: add seed to populate the db

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,56 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function genereateIsbn (): string {
+  const isbn = Math.floor(
+    Math.random() * (9999999999999 - 1) + 1
+  )
+
+  return isbn.toString()
+}
+
+const userData: Prisma.bookUncheckedCreateInput[] = [
+  {
+    isbn: genereateIsbn(),
+    name: 'lorem ipsun',
+    author: 'jhon doe',
+    pages: 100,
+    copies: 10
+  },
+  {
+    isbn: genereateIsbn(),
+    name: 'title of a book',
+    author: 'xpto',
+    pages: 200,
+    copies: 20
+  },
+  {
+    isbn: genereateIsbn(),
+    name: 'second title of a book',
+    author: 'jk',
+    pages: 300,
+    copies: 30
+  }
+]
+
+async function main (): Promise<void> {
+  console.log('Start seeding ...')
+  for (const u of userData) {
+    const book = await prisma.book.create({
+      data: u
+    })
+    console.log(`Created book with id: ${book.isbn}`)
+  }
+  console.log('Seeding finished.')
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
