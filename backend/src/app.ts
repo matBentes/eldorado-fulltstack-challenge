23e42220e268260337bb1b9729c9844276fa303c
Mateus Bentes <mateus1337bentes@gmail.com>
2022-10-21 16:15:42 -0400

    feat: add prisma and book crud

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,62 @@
-console.log('started')
+import express from 'express'
+import dotenv from 'dotenv'
+import { PrismaClient } from '@prisma/client'
+
+const PORT = process.env.NODE_PORT
+
+const prisma = new PrismaClient()
+dotenv.config()
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server')
+})
+
+app.post('/book', async (req, res) => {
+  const {
+    isbn,
+    name,
+    author,
+    copies,
+    pages
+  } = req.body
+
+  await prisma.book.create({
+    data: {
+      isbn,
+      name,
+      author,
+      copies,
+      pages
+    }
+  })
+
+  // TODO return the correct status code
+})
+
+app.delete('/book/:isbn', async (req, res) => {
+  const { isbn } = req.params
+
+  await prisma.book.delete({
+    where: {
+      isbn: +isbn
+    }
+  })
+
+  // TODO return the correct status code
+})
+
+app.put('/book/:isbn', async (req, res) => {
+  const { isbn } = req.params
+  const { author } = req.body
+
+  await prisma.book.update({
+    where: { isbn: +isbn },
+    data: { author }
+  })
+  // TODO return the correct status code
+})
+
+app.listen(PORT, () => {
+  console.log('🚀 Server ready at: http://localhost:3333')
+})
