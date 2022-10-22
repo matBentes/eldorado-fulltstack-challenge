a38c39f719612c26f826d258d61388d5aa3150d5
Mateus Bentes <mateus1337bentes@gmail.com>
2022-10-21 23:47:59 -0400

    test: ensure many books will be returned

--- backend/src/app.spec.ts
+++ backend/src/app.spec.ts
@@ -14,9 +14,35 @@ afterAll(async () => {
 describe('GET /book', () => {
   it('should get a empty array of books', async () => {
     const { body, status } = await request(app).get('/book')
-    console.log(body)
 
     expect(body).toStrictEqual([])
+    expect(status).toBe(200)
+  })
+
+  it('should return many books', async () => {
+    await prisma.book.createMany({
+      data: [{
+        isbn: '1234',
+        name: 'lorem ipsun',
+        author: 'jhon doe',
+        pages: 100,
+        copies: 10
+      },
+      {
+        isbn: '456',
+        name: 'title of a book',
+        author: 'xpto',
+        pages: 200,
+        copies: 20
+      }]
+    })
+
+    const { body, status } = await request(app).get('/book')
+
+    expect(body).toEqual(expect.arrayContaining([
+      expect.objectContaining({ isbn: '456', author: 'xpto' }),
+      expect.objectContaining({ isbn: '1234', author: 'jhon doe' })
+    ]))
     expect(status).toBe(200)
   })
 })
