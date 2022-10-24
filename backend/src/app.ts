2c25ee10446663b02e772b58690789a664c2e486
Mateus Bentes <36206555+matBentes@users.noreply.github.com>
2022-10-23 22:37:05 -0400

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import { app } from './server'
 
-const PORT = process.env.NODE_PORT
+const PORT = 3333 || process.env.NODE_PORT
 
 app.listen(PORT, () => {
   console.log('🚀 Server ready at: http://localhost:3333')
