7e38bacb975652ee5753f98a85ca1e7a5d081ba9
Mateus Bentes <36206555+matBentes@users.noreply.github.com>
2022-12-12 20:09:19 -0400

    Update Book.ts

--- backend/src/controller/Book.ts
+++ backend/src/controller/Book.ts
@@ -1,9 +1,10 @@
+import { book } from '@prisma/client'
 import { Request, Response } from 'express'
 import { prisma } from '../server'
 import { genereateIsbn } from '../utils'
 
 class Book {
-  async listAll (req: Request, res: Response): Promise<Response> {
+  async listAll(req: Request, res: Response): Promise<Response> {
     try {
       const books = await prisma.book.findMany()
       return res.json(books).status(200)
@@ -12,10 +13,50 @@ class Book {
     }
   }
 
-  async insert (req: Request, res: Response): Promise<Response> {
+  async insert(req: Request, res: Response): Promise<Response> {
     const isbn = genereateIsbn()
     try {
-      const { name, author, copies, pages } = req.body
+      const { name, author, copies, pages } = req.body as book
+
+      if (!name) {
+        return res.status(400).json({ error: "name não pode ser vazio ou nulo" })
+      }
+
+      if (name.length > 100) {
+        return res.status(400).json({ error: "name não pode exceder 100 caracteres" })
+      }
+
+      if(!name[0].match(/^[a-zA-Z1-9]$/)) {
+        return res.status(400).json({ error: "O primeiro caractere de name deve ser uma letra ou número." })
+      }
+
+      if (!author) {
+        return res.status(400).json({ error: "author não pode ser vazio ou nulo" })
+      }
+
+      if (author.length > 100) {
+        return res.status(400).json({ error: "name não pode exceder 100 caracteres" })
+      }
+
+      if(!author[0].match(/^[a-zA-Z]$/)) {
+        return res.status(400).json({ error: "O primeiro caractere de author deve ser uma letra ou número." })
+      }
+      
+      if(typeof pages != 'number' || pages == 0) {
+        return res.status(400).json({ error: "pages deve ser um inteiro maior que zero." })
+      }
+
+      if(typeof pages == 'number' && copies > 2147483647 ) {
+        return res.status(400).json({ error: "pages deve possuir um valor máximo de até 2147483647." })
+      }
+
+      if(typeof copies != 'number' || copies == 0) {
+        return res.status(400).json({ error: "copies deve ser um inteiro maior que zero." })
+      }
+
+      if(typeof copies == 'number' && copies > 2147483647 ) {
+        return res.status(400).json({ error: "copies deve possuir um valor máximo de até 2147483647." })
+      }
 
       const newBook = await prisma.book.create({
         data: {
@@ -29,13 +70,23 @@ class Book {
 
       return res.status(201).json(newBook)
     } catch (error) {
-      return res.status(404)
+      return res.status(500)
     }
   }
 
-  async delete (req: Request, res: Response): Promise<Response> {
+  async delete(req: Request, res: Response): Promise<Response> {
     try {
       const { isbn } = req.params
+
+      const book = await prisma.book.findFirst({
+        where: {
+          isbn
+        }
+      })
+
+      if (!book) {
+        return res.status(404).json({ error: "livro não encontrado" })
+      }
 
       await prisma.book.delete({
         where: {
@@ -49,7 +100,7 @@ class Book {
     }
   }
 
-  async update (req: Request, res: Response): Promise<Response> {
+  async update(req: Request, res: Response): Promise<Response> {
     try {
       const { isbn } = req.params
       const { name, author, copies, pages } = req.body
@@ -70,7 +121,7 @@ class Book {
     }
   }
 
-  async findOne (req: Request, res: Response): Promise<Response> {
+  async findOne(req: Request, res: Response): Promise<Response> {
     try {
       const { isbn } = req.params
 
@@ -80,7 +131,11 @@ class Book {
         }
       })
 
-      return res.status(201).json(book)
+      if (!book) {
+        return res.status(404).json({ error: "livro não encontrado" })
+      }
+
+      return res.status(200).json(book)
     } catch (e) {
       return res.status(500)
     }
